ae0adc5fd959a7ea2be37bf06067817b6abf43f1
Siarhei Shumilin <siarhei_shumilin@epam.com>
2020-01-12 21:45:36 +0300

    changed SpringAppConfig for connection pool

--- web/src/main/java/com/epam/esm/config/SpringAppConfig.java
+++ web/src/main/java/com/epam/esm/config/SpringAppConfig.java
@@ -1,9 +1,10 @@
 package com.epam.esm.config;
 
+import com.epam.esm.dao.connection.ConnectionPool;
+import com.epam.esm.dao.connection.ManagerDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
@@ -15,11 +16,6 @@ public class SpringAppConfig {
 
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/gift_certificates?serverTimezone=Europe/Moscow&allowPublicKeyRetrieval=true&useSSL=false");
-        dataSource.setUsername("root");
-        dataSource.setPassword("12345");
-        return dataSource;
+        return new ManagerDataSource(ConnectionPool.getInstance());
     }
 }
